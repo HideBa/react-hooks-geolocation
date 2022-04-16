7f5b9fae5732cf1536a1926221fd94f8649c8fef
HideBa <baba.papa1120.ba@gmail.com>
2022-04-16 14:17:29 +0900

    impl example and geolocation api

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,57 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from "../src"
+import { useGeolocation } from "../src"
+import  { Box, Button, ChakraProvider, Flex, HStack, Spinner, Table, TableCaption, TableContainer, TabPanel, Tbody, Th, Thead, Tr, VStack } from "@chakra-ui/react"
 
 const App = () => {
+  const {location,loading,metaInfo, activate, update, watch, unwatch} = useGeolocation({defaultActive: false});
   return (
-    <div>
-      <Thing />
-    </div>
+    <ChakraProvider>
+      <Box m="10">
+        <Flex direction="column" align="center">
+          {
+            loading ? (<Flex direction="column" align="center" m="10">
+              <div>loading</div>
+              <Spinner color='red'/>
+            </Flex>
+            )
+             : (
+            <TableContainer>
+              <Table variant="simple">
+                <Thead>
+                  <Tr>
+                    <Th>Value1</Th>
+                    <Th>Value2</Th>
+                    <Th>Value3</Th>
+                    <Th>Value4</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Th>lat: {location?.latitude ?? "N/A"}</Th>
+                    <Th>lng: {location?.longitude ?? "N/A"}</Th>
+                    <Th>alt: {location?.altitude ?? "N/A"}</Th>
+                  </Tr>
+                  <Tr>
+                    <Th>accuracy: {metaInfo?.accuracy ?? "N/A"}</Th>
+                    <Th>altitudeAccuracy: {metaInfo?.altitudeAccuracy ?? "N/A"}</Th>
+                    <Th>heading: {metaInfo?.heading ?? "N/A"}</Th>
+                    <Th>speed: {metaInfo?.speed ?? "N/A"}</Th>
+                  </Tr>
+                </Tbody>
+              </Table>
+            </TableContainer>
+            )
+          }
+          <VStack>
+            <Button onClick={activate}>Activate geolocation API</Button>
+            <Button onClick={update}>Update location</Button>
+            <Button onClick={watch}>Watch location change</Button>
+            <Button onClick={unwatch}>Unwatch location change</Button>
+          </VStack>
+        </Flex>
+      </Box>
+    </ChakraProvider>
   );
 };
 
--- src/use-geolocation.ts
+++ src/use-geolocation.ts
@@ -6,7 +6,7 @@ export type Location = {
   altitude:number|null;
 }
 
-type LocationMetaInfo = {
+type MetaInfo = {
   altitudeAccuracy: number|null;
   accuracy: number|null;
   heading: number|null
@@ -14,61 +14,105 @@ type LocationMetaInfo = {
 }
 
 
-type ErrType = "userDeny"
+type ErrType = "PERMISSION_DENIED"|"POSITION_UNAVAILABLE"|"TIMEOUT"|"OTHER"
 
 type Err  = {
   type: ErrType,
   message: string
 }
 
+type Args = {
+  defaultActive?: boolean;
+  onGeolocationChange?: (location?: Location, metaInfo?: MetaInfo) => void;
+  geolocation?: Geolocation
+}
 
-export default ({onGeoLocationApiActive, active}:{onGeoLocationApiActive: () => void, active: boolean}) => {
-  const [isControlledGeoLocationApiActive, activateControlledGeoLocationApi] = useState(false);
-  const isActive = isControlledGeoLocationApiActive || active;
-  const [currentLocation, setCurrentLocation] = useState<Location>();
-  const [locationMetaInfo, setLocationMetaInfo] = useState<LocationMetaInfo>();
-  const [locationErr, setLocationErr] = useState<string>();
-  const [isGettingLocation, setIsGettingLocation] = useState(false);
+
+export default ({geolocation = navigator.geolocation, defaultActive = true, onGeolocationChange}:Args) => {
+  const [isActive, setActivate] = useState(defaultActive);
+  const [location, setLocation] = useState<Location>();
+  const [metaInfo, setMetaInfo] = useState<MetaInfo>();
+  const [locationErr, setLocationErr] = useState<Err>();
+  const [loading, setLoading] = useState(false);
+
+  let watchId:number|undefined = undefined;
+
+
+  //Fetch geolocation via geolocation API
   useEffect(() => {
     if (!isActive) return;
-    navigator.geolocation.getCurrentPosition(
-      (pos: GeolocationPosition) => {
-        setCurrentLocation({
-          latitude: pos.coords.latitude,
-          longitude: pos.coords.longitude,
-          altitude:pos.coords.altitude
-        });
-        setLocationMetaInfo({
-          altitudeAccuracy: pos.coords.altitudeAccuracy,
-          accuracy: pos.coords.accuracy,
-          heading: pos.coords.heading,
-          speed: pos.coords.speed
-      }),
-      (posErr: GeolocationPositionError) => {
-        setLocationErr(posErr.message);
-      },
-    );
+    update();
   }, [isActive]);
 
-  const handleGeoLocationGet = useCallback(() => {
-    console.log(`click`);
-    activateControlledGeoLocationApi(true);
-    setIsGettingLocation(true);
-  }, [activateControlledGeoLocationApi, setIsGettingLocation]);
-
   useEffect(() => {
-    if (!isActive || !currentLocation) return;
-    setIsGettingLocation(false);
-  }, [setIsGettingLocation, isActive, currentLocation]);
+    onGeolocationChange?.(location, metaInfo);
+  }, [onGeolocationChange, location, metaInfo])
 
-  useEffect(() => {
-    console.log(currentLocation);
-  }, [currentLocation]);
+  const handleSetData = useCallback((pos: GeolocationPosition) => {
+    setLocation({
+      latitude: pos.coords.latitude,
+      longitude: pos.coords.longitude,
+      altitude:pos.coords.altitude,
+    });
+    setMetaInfo({
+      altitudeAccuracy: pos.coords.altitudeAccuracy,
+      accuracy: pos.coords.accuracy,
+      heading: pos.coords.heading,
+      speed: pos.coords.speed,
+    });
+    setLoading(false);
+    setLocationErr(undefined);
+  }, [])
 
+  const handleErr = useCallback((err: GeolocationPositionError) => {
+    setLocation(undefined);
+    setMetaInfo(undefined);
+    setLoading(false)
+    setLocationErr({type: convertErr(err.code), message: err.message});
+  }, [])
+
+  const activate = useCallback(() => {
+    setActivate(true)
+  }, [])
+
+  const update = useCallback(() => {
+    setLoading(true);
+    geolocation.getCurrentPosition(
+      handleSetData,
+      handleErr
+    )
+  }, [geolocation])
+
+  const watch = useCallback(() => {
+    watchId = geolocation.watchPosition(handleSetData, handleErr)
+  }, [geolocation, handleSetData, handleErr])
+
+  const unwatch = useCallback(() => {
+    if(typeof watchId === "undefined")return;
+    geolocation.clearWatch(watchId)
+  }, [watchId, geolocation])
 
   return {
-    handleGeoLocationGet,
-    currentLocation,
-    loading: isGettingLocation,
+    activate,
+    update,
+    watch,
+    unwatch,
+    metaInfo,
+    locationErr,
+    location,
+    loading,
   };
+}
+
+const convertErr = (errCode:number): ErrType => {
+  switch (errCode) {
+    case 1:
+      return "PERMISSION_DENIED"
+    case 2:
+      return "POSITION_UNAVAILABLE"
+      case 3:
+        return "TIMEOUT"
+    default:
+      return "OTHER"
+  }
 }
